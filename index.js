1cb33cba69700f789842912a2c28a5b679aa9776
bimaindrawans <147312348+bimaindrawans@users.noreply.github.com>
2023-11-17 13:17:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { EmbedBuilder, IntentsBitField, Client } from "discord.js";
-import { PrismaClient } from "@prisma/client";
+import { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, Client, IntentsBitField } from 'discord.js';
+import { PrismaClient } from '@prisma/client';
 
 const client = new Client({
     intents: [
@@ -12,10 +12,36 @@ const client = new Client({
 });
 
 const prisma = new PrismaClient();
+const botsPerPage = 10;
 
-client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
+// Helper function to create bot embed
+async function createBotEmbed(guild, bots, page) {
+    const startIndex = page * botsPerPage;
+    const endIndex = startIndex + botsPerPage;
+    const botsOnPage = bots.slice(startIndex, endIndex);
+
+    const botsStatus = await Promise.all(botsOnPage.map(async bot => {
+        const voice = guild.voiceStates.cache.get(bot.bot_id);
+        const b = await guild.members.fetch({ user: bot.bot_id }).catch(() => ({ displayName: "Unknown Bot" }));
+        return voice ? `❎ | ${b.displayName}` : `✅ | ${b.displayName}`;
+    }));
+
+    const availableCount = botsStatus.filter(status => status.startsWith("✅")).length;
+    const unavailableCount = botsStatus.filter(status => status.startsWith("❎")).length;
+
+    const embed = new EmbedBuilder()
+        .setAuthor({
+            name: `${guild.name} Available Music Bots (Page ${page + 1})`,
+            iconURL: guild.iconURL({ size: 4096 })
+        })
+        .setDescription(botsStatus.join("\n"))
+        .addFields(
+            { name: "✅ Total Available on Page", value: availableCount.toString(), inline: true },
+            { name: "❌ Total Unavailable on Page", value: unavailableCount.toString(), inline: true }
+        );
+
+    return embed;
+}
 
 client.on("messageCreate", async message => {
     const prefix = process.env.PREFIX;
@@ -31,48 +57,73 @@ client.on("messageCreate", async message => {
         });
 
         if (!bots.length) {
-            return message.reply({
-                content: "There is no music bot in this server.", failIfNotExists: false
-            });
+            return message.reply("There are no music bots in this server.");
         }
 
-        const botsStatus = await Promise.all(bots.map(async bot => {
-            const voice = message.guild.voiceStates.cache.get(bot.bot_id);
-            const b = await message.guild.members.fetch({ user: bot.bot_id }).catch(() => ({ displayName: "Unknown Bot" }));
-            if (voice) return `❎ | ${b.displayName}`;
-            return `✅ | ${b.displayName}`;
-        }));
+        const embed = await createBotEmbed(message.guild, bots, 0);
 
-        const availableCount = botsStatus.filter(status => status.startsWith("✅")).length;
-        const unavailableCount = botsStatus.filter(status => status.startsWith("❎")).length;
-
-        const embed = new EmbedBuilder()
-            .setAuthor({
-                name: `${message.guild.name} Available Music Bots`,
-                iconURL: message.guild.iconURL({ size: 4096 })
-            })
-            .setDescription(botsStatus.join("\n"))
-            .addFields(
-                {
-                    name: "✅ Total Available",
-                    value: availableCount.toString(),
-                    inline: true
-                },
-                {
-                    name: "❌ Total Unavailable",
-                    value: unavailableCount.toString(),
-                    inline: true
-                }
+        const row = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId('previous_page')
+                    .setLabel('Previous')
+                    .setStyle(ButtonStyle.Primary)
+                    .setDisabled(true),
+                new ButtonBuilder()
+                    .setCustomId('next_page')
+                    .setLabel('Next')
+                    .setStyle(ButtonStyle.Primary)
+                    .setDisabled(bots.length <= botsPerPage)
             );
 
-        return message.reply({ embeds: [embed], failIfNotExists: false });
+        const botMessage = await message.reply({ embeds: [embed], components: [row] });
+        const collector = botMessage.createMessageComponentCollector({ time: 60000 });
+
+        collector.on('collect', async i => {
+            if (!i.isButton()) return;
+            if (i.user.id !== message.author.id) {
+                return i.reply({ content: 'You cannot use this button.', ephemeral: true });
+            }
+
+            let currentPage = parseInt(i.message.embeds[0].author.name.match(/Page (\d+)/)[1], 10) - 1;
+
+            if (i.customId === 'previous_page') {
+                currentPage = Math.max(0, currentPage - 1);
+            } else if (i.customId === 'next_page') {
+                currentPage = Math.min(Math.ceil(bots.length / botsPerPage) - 1, currentPage + 1);
+            }
+
+            const newEmbed = await createBotEmbed(message.guild, bots, currentPage);
+            const newRow = new ActionRowBuilder()
+                .addComponents(
+                    new ButtonBuilder()
+                        .setCustomId('previous_page')
+                        .setLabel('Previous')
+                        .setStyle(ButtonStyle.Primary)
+                        .setDisabled(currentPage === 0),
+                    new ButtonBuilder()
+                        .setCustomId('next_page')
+                        .setLabel('Next')
+                        .setStyle(ButtonStyle.Primary)
+                        .setDisabled((currentPage + 1) * botsPerPage >= bots.length)
+                );
+
+            await i.update({ embeds: [newEmbed], components: [newRow] });
+        });
+
+        collector.on('end', collected => botMessage.edit({ components: [] }));
     }
 
     if (command === "addbot") {
-        if (!message.member.permissions.has("ManageGuild")) return;
+        if (!message.member.permissions.has("ManageGuild")) {
+            return message.reply("You do not have permission to manage bots.");
+        }
+
         const bot = message.mentions.members.first();
-        if (!bot) return message.reply({ content: "Please mention a bot.", failIfNotExists: false });
-        if (!bot.user.bot) return message.reply({ content: "Please mention a bot.", failIfNotExists: false });
+        if (!bot || !bot.user.bot) {
+            return message.reply("Please mention a valid bot.");
+        }
+
         const botExists = await prisma.bot.findFirst({
             where: {
                 bot_id: bot.id,
@@ -80,7 +131,10 @@ client.on("messageCreate", async message => {
             }
         });
 
-        if (botExists) return message.reply({ content: "This bot is already added.", failIfNotExists: false });
+        if (botExists) {
+            return message.reply("This bot is already added.");
+        }
+
         await prisma.bot.create({
             data: {
                 bot_id: bot.id,
@@ -88,14 +142,19 @@ client.on("messageCreate", async message => {
             }
         });
 
-        return message.reply({ content: "Bot added successfully.", failIfNotExists: false });
+        return message.reply("Bot added successfully.");
     }
+       // Command to remove a bot
+       if (command === "removebot") {
+        if (!message.member.permissions.has("ManageGuild")) {
+            return message.reply("You do not have permission to manage bots.");
+        }
 
-    if (command === "removebot") {
-        if (!message.member.permissions.has("ManageGuild")) return;
         const bot = message.mentions.members.first();
-        if (!bot) return message.reply({ content: "Please mention a bot.", failIfNotExists: false });
-        if (!bot.user.bot) return message.reply({ content: "Please mention a bot.", failIfNotExists: false });
+        if (!bot || !bot.user.bot) {
+            return message.reply("Please mention a valid bot.");
+        }
+
         const botExists = await prisma.bot.findFirst({
             where: {
                 bot_id: bot.id,
@@ -103,15 +162,18 @@ client.on("messageCreate", async message => {
             }
         });
 
-        if (!botExists) return message.reply({ content: "This bot is not added.", failIfNotExists: false });
+        if (!botExists) {
+            return message.reply("This bot is not in the list.");
+        }
+
         await prisma.bot.delete({
             where: {
                 id: botExists.id
             }
         });
 
-        return message.reply({ content: "Bot removed successfully.", failIfNotExists: false });
+        return message.reply("Bot removed successfully.");
     }
 });
 
-await client.login();
+await client.login(process.env.BOT_TOKEN);
